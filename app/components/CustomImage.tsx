99b518986baad77166a7f7c7c8bf79ec2df019a5
tkfjysft <tkfjysft.2013.frgn@gmail.com>
2026-07-05 20:00:06 +0900

    fix:サブディレクトリをfに変更

--- app/components/CustomImage.tsx
+++ app/components/CustomImage.tsx
@@ -2,8 +2,8 @@ import NextImage, { ImageProps } from 'next/image';
 
 export default function CustomImage({ src, ...props }: ImageProps) {
   // srcが文字列の場合のみパス調整を行う
-  const patchedSrc = typeof src === 'string' && src.startsWith('/') && !src.startsWith('/ec1/')
-    ? `/ec1${src}`
+  const patchedSrc = typeof src === 'string' && src.startsWith('/') && !src.startsWith('/f/')
+    ? `/f${src}`
     : src;
 
   return <NextImage src={patchedSrc} {...props} />;
